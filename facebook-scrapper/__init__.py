d0d19e112197b51c9e622fcc3ce1ee75721367d4
Aliaksandr Fedaryna <aliaksandr.fedaryna@ihsmarkit.com>
2022-12-18 22:47:05 +0100

    feat: add messages posting

--- facebook-scrapper/__init__.py
+++ facebook-scrapper/__init__.py
@@ -1,28 +1,62 @@
 import datetime
-import logging
 import os
 import csv
 import io
 import azure.functions as func
 import azure.storage.blob as blob
+from facebook_scraper import get_posts
+import telegram
 
 PAGES = "pages.csv"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+FIELDS = ["telegram_name", "fbpage_tag", "last_post_date"]
+MAX_COUNT_FLOOD_CONTROL = 20
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
     if mytimer.past_due:
-        logging.info('The timer is past due!')
+        return
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
     connection_string = os.getenv("AZURE_STORAGE_BLOB_CONNECTIONSTRING")
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+    bot = telegram.bot.Bot(token)
+
     service = blob.BlobServiceClient.from_connection_string(connection_string)
     blob_client = service.get_container_client(container="configuration")
+    writer_steram = io.StringIO()
 
     pagesContent = blob_client.download_blob(PAGES).content_as_text()
-    stream = io.StringIO(pagesContent)
-    pages_reader = csv.DictReader(stream)
-    
+    pages_reader = csv.DictReader(io.StringIO(pagesContent))
+    pages_writer = csv.DictWriter(writer_steram, FIELDS)
+    pages_writer.writeheader()
+
+    messages = 0
     for page in pages_reader:
-        logging.info("%s -> %s", page["page_name"], page["page_tag"])
+        posts = list(get_posts(page["fbpage_tag"], page_limit=2))
+        posts.sort(key = lambda x: x["time"])
+        last_post_dt = datetime.datetime.strptime(page["last_post_date"], DATE_FORMAT)
+
+        for post in posts:
+            if messages >= MAX_COUNT_FLOOD_CONTROL: # next time will be the rest
+                break
+
+            if post["time"] <= last_post_dt: # skip this one
+                continue
+
+            if post["image"] is not None or post["image_lowquality"] is not None:
+                bot.send_photo(chat_id, post["image"] if post["image"] else post["image_lowquality"], (post['text'] if post['text'] else ''))
+                if (post['time'] > last_post_dt):
+                    last_post_dt = post['time']
+            elif post["text"] is not None:
+                bot.send_message(chat_id, (post['text'] if post['text'] else ''))
+                if (post['time'] > last_post_dt):
+                    last_post_dt = post['time']
+
+            messages += 1
+
+        row = {'telegram_name': page['telegram_name'], 'fbpage_tag': page['fbpage_tag'], 'last_post_date': last_post_dt}
+        pages_writer.writerow(row)
+
+    writer_steram.flush()
+    blob_client.upload_blob(name = PAGES, data = writer_steram.getvalue(), overwrite = True)
