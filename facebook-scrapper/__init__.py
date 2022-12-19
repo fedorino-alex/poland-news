8e55782c6a35b0643266bee3864d2e6c59d6750b
Aliaksandr Fedaryna <aliaksandr.fedaryna@ihsmarkit.com>
2022-12-20 00:59:49 +0100

    feat: add saving content of articles

--- facebook-scrapper/__init__.py
+++ facebook-scrapper/__init__.py
@@ -2,10 +2,14 @@ import datetime
 import os
 import csv
 import io
+import time
+import random
 import azure.functions as func
 import azure.storage.blob as blob
-from facebook_scraper import get_posts
+from facebook_scraper import get_posts, fb_types
 import telegram
+import trafilatura
+from urllib.parse import urlparse
 
 PAGES = "pages.csv"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -16,6 +20,8 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         return
 
+    trafilatura.core.HAS_SIGNAL = False # disable signal
+
     connection_string = os.getenv("AZURE_STORAGE_BLOB_CONNECTIONSTRING")
     token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
@@ -24,6 +30,8 @@ def main(mytimer: func.TimerRequest) -> None:
 
     service = blob.BlobServiceClient.from_connection_string(connection_string)
     blob_client = service.get_container_client(container="configuration")
+    pages_client = service.get_container_client(container="pages")
+
     writer_steram = io.StringIO()
 
     pagesContent = blob_client.download_blob(PAGES).content_as_text()
@@ -33,30 +41,46 @@ def main(mytimer: func.TimerRequest) -> None:
 
     messages = 0
     for page in pages_reader:
-        posts = list(get_posts(page["fbpage_tag"], page_limit=2))
-        posts.sort(key = lambda x: x["time"])
         last_post_dt = datetime.datetime.strptime(page["last_post_date"], DATE_FORMAT)
 
-        for post in posts:
-            if messages >= MAX_COUNT_FLOOD_CONTROL: # next time will be the rest
-                break
+        if messages < MAX_COUNT_FLOOD_CONTROL:
+            posts = list(get_posts(page["fbpage_tag"], page_limit=2, options={ "posts_per_page": 20, "allow_extra_requests": False }))
+            posts.sort(key = lambda x: x["time"])
 
-            if post["time"] <= last_post_dt: # skip this one
-                continue
+            for post in posts:
+                if messages >= MAX_COUNT_FLOOD_CONTROL:
+                    break
 
-            if post["image"] is not None or post["image_lowquality"] is not None:
-                bot.send_photo(chat_id, post["image"] if post["image"] else post["image_lowquality"], (post['text'] if post['text'] else ''))
-                if (post['time'] > last_post_dt):
-                    last_post_dt = post['time']
-            elif post["text"] is not None:
-                bot.send_message(chat_id, (post['text'] if post['text'] else ''))
-                if (post['time'] > last_post_dt):
-                    last_post_dt = post['time']
+                if post["time"] <= last_post_dt: # skip this one
+                    continue
 
-            messages += 1
+                _save_post(page, post, pages_client)
+
+                if post["image"] is not None or post["image_lowquality"] is not None:
+                    bot.send_photo(chat_id, post["image"] if post["image"] else post["image_lowquality"], (post['text'] if post['text'] else ''))
+                    if (post['time'] > last_post_dt):
+                        last_post_dt = post['time']
+                elif post["text"] is not None:
+                    bot.send_message(chat_id, (post['text'] if post['text'] else ''))
+                    if (post['time'] > last_post_dt):
+                        last_post_dt = post['time']
+
+                messages += 1
+
+            time.sleep(random.Random().randint(5,25)) # waiting between scrolls
 
         row = {'telegram_name': page['telegram_name'], 'fbpage_tag': page['fbpage_tag'], 'last_post_date': last_post_dt}
         pages_writer.writerow(row)
 
     writer_steram.flush()
     blob_client.upload_blob(name = PAGES, data = writer_steram.getvalue(), overwrite = True)
+
+def _save_post(page, post: fb_types.Post, pages_client: blob.BlobClient):
+    try:
+        page_content = trafilatura.fetch_url(post['link'])
+        content = trafilatura.extract(page_content, favor_precision=True, include_formatting=True)
+    except Exception:
+        return
+
+    link = urlparse(post['link'])
+    pages_client.upload_blob(name = page['fbpage_tag'] + "/" + link.path.replace('/','_') , data = content, overwrite = True)
